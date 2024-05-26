cf3a7cb96ed861d04b70ec4ec88a27b5eba7276f
SyedaIlmaAli <160448031+SyedaIlmaAli@users.noreply.github.com>
2024-05-27 03:08:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+// Day 01
+// Question 02
+let firstName = "Ilma";
+console.log(firstName);
+// Question 03
+let secondName = "Amna";
+console.log(secondName.toUpperCase());
+console.log(secondName.toLowerCase());
+console.log(secondName.charAt(0).toUpperCase() + secondName.slice(1).toLowerCase());
---
+++ index.ts
@@ -0,0 +1,13 @@
+// Day 01
+
+// Question 02
+
+let firstName : string = "Ilma";
+console.log(firstName);
+
+// Question 03
+
+let secondName : string = "Amna";
+console.log(secondName.toUpperCase());
+console.log(secondName.toLowerCase());
+console.log(secondName.charAt(0).toUpperCase() + secondName.slice(1).toLowerCase());
